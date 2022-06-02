8e2c603b0f7fb7ba26252e32d39891aedeb423e9
LEEWONHOI <dldnjsghl92@gmail.com>
2022-06-02 14:59:12 +0900

    basic-login 실습

--- server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,21 +1,83 @@
 package com.sp.fc.web.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final CustomAuthDetails customAuthDetails;
+
+    public SecurityConfig(CustomAuthDetails customAuthDetails) {
+        this.customAuthDetails = customAuthDetails;
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("user1")
+                                .password("1111")
+                                .roles("USER")
+                ).withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("admin")
+                                .password("2222")
+                                .roles("ADMIN")
+                );
+    }
+
+    // ADMIN 은 USER 권한을 전부 가질 수 있도록 함
+    @Bean
+    RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(request->{
+                .authorizeRequests(request -> {
                     request
-                            .antMatchers("/**").permitAll()
-                            ;
+                            .antMatchers("/").permitAll()
+                            .anyRequest().authenticated()   // 어떠한 요청도 접근 권한이 있어야한다 설정
+                    ;
                 })
-                ;
+                .formLogin(
+                        login -> login.loginPage("/login")
+                                .permitAll()    // 권한 ok 를 해줘야 login page 에 접근할 수 있음. (없을 시 무한루프)
+                                .defaultSuccessUrl("/", false)  // false 로 해야 성공 시에도 루트 페이지 이동을 막을 수 있다.
+                                .failureUrl("/login-error") // 로그인 실패시 해당 url 로 이동
+                                .authenticationDetailsSource(customAuthDetails)
+                )
+                // 로그아웃 성공시 루트 페이지로 이동
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                // 권한 오류 등 에러 발생 시 에러 페이지로 이등
+                .exceptionHandling(exception -> exception.accessDeniedPage("/access-denied"))
+        ;
+
+    }
+
+    // static data 를 무시하도록 설정
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
     }
 }
