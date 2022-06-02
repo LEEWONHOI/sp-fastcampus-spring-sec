b8a0bd1fc5c914bea92a9a44264b8f6462c62201
LEEWONHOI <dldnjsghl92@gmail.com>
2022-06-02 18:04:25 +0900

    httpBasic 을 사용하여 SPA 등과 같은 페이지에서 인증 처리 및 csrf 문제점 발생

--- server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
+++ server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
@@ -1,6 +1,74 @@
 package com.sp.fc.web;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BasicAuthenticationTest {
 
-    
+    @LocalServerPort
+    int port;
+
+    RestTemplate client = new RestTemplate();
+
+    private String greetingUrl() {
+        return "http://localhost:" + port + "/greeting";
+    }
+
+    @Test
+    @DisplayName("1. 인증 실패")
+    public void test1(){
+        assertThatThrownBy(() -> {
+            client.getForObject(greetingUrl(), String.class);
+        }).isInstanceOf(HttpClientErrorException.class);
+    }
+
+    @Test
+    @DisplayName("2. 인증 성공")
+    public void test2() throws Exception {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Basic " + Base64.getEncoder().encodeToString(
+                "user1:1111".getBytes()
+        ));
+        HttpEntity entity = new HttpEntity(null, headers);
+
+        ResponseEntity<String> resp = client.exchange(greetingUrl(), HttpMethod.GET, entity, String.class);
+
+        assertThat(resp.getBody()).isEqualTo("hello");
+    }
+
+    @Test
+    @DisplayName("3. 인증성공2")
+    public void test3() throws Exception {
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        String resp = testClient.getForObject(greetingUrl(), String.class);
+        assertThat(resp).isEqualTo("hello");
+    }
+
+    @Test
+    @DisplayName("4. POST 인증")
+    public void test4() throws Exception {
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        ResponseEntity<String> resp = testClient.postForEntity(greetingUrl(), "lee", String.class);
+        assertThat(resp.getBody()).isEqualTo("hello lee");
+
+    }
+
+
 }
